9e3afaa115220eb23c032f24decff54cdf0adfe7
Abdu Musowir U <musowir@pg.cusat.ac.in>
2022-11-23 23:37:10 +0530

    commended progress of combined results

--- sarg/views.py
+++ sarg/views.py
@@ -11,17 +11,17 @@ def catsel(request):
     cat = Catogory.objects.get(catogory_name=cat)
     res = Result.objects.all()
     com = Competition.objects.filter(catogory=cat)
-    resul = []
-    unit = Unit.objects.all()
-    for r in res:
-        if r.competition.catogory.catogory_name == cat:
-            resul.append(r)
-    print(resul)
-    rc=[]
-    for u in unit:
-        for c in com:
-            rc.append([r if r.competition==c and r.unit==u else 0 for r in resul])
-    print(rc)
+    # resul = []
+    # unit = Unit.objects.all()
+    # for r in res:
+    #     if r.competition.catogory.catogory_name == cat:
+    #         resul.append(r)
+    # print(resul)
+    # rc=[]
+    # for u in unit:
+    #     for c in com:
+    #         rc.append([r if r.competition==c and r.unit==u else 0 for r in resul])
+    # print(rc)
                     
 
     com = Competition.objects.filter(catogory=cat)
